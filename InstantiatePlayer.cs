9d19785e859a5394b69daa7f31bf0c13dfd115fe
MINAMI <c5766tkm@gmail.com>
2018-07-25 18:41:11 +0900

    first commit

---
+++ InstantiatePlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using MonobitEngine;
+using System;
+
+public class InstantiatePlayer : MonobitEngine.MonoBehaviour {
+
+	// 自身がルームに入室を試みたかどうかのフラグ
+	bool isJoinOrCreateRoom = false;
+
+	// 自身のプレイヤーキャラクタが生成されたかどうかのフラグ
+	bool isMakeMyPlayer = false;
+	// Use this for initialization
+	void Start () {
+		// MUNサーバに接続する
+		MonobitNetwork.autoJoinLobby = true;
+		MonobitNetwork.ConnectServer("v0.1.1");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// 未接続状態なら何もしない
+		if (!MonobitNetwork.isConnect)
+		{
+			return;
+		}
+		// ルーム入室処理未実行であれば、ルームの作成or入室をする
+		if (!isJoinOrCreateRoom)
+		{
+			MonobitNetwork.JoinOrCreateRoom("MonobitTest", new RoomSettings(), null);
+			isJoinOrCreateRoom = true;
+			return;
+		}
+		// ルーム入室済みでキャラクタ未生成であれば、キャラクタを生成する
+		if(MonobitNetwork.inRoom && !isMakeMyPlayer)
+		{
+			MonobitNetwork.Instantiate ("Player", Vector3.zero, Quaternion.identity, 0);
+			isMakeMyPlayer = true;
+			return;
+		}
+	}
+}
---
+++ Tracking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracking : MonobitEngine.MonoBehaviour {
+	public GameObject Target;
+	public string TargetName;
+	// Use this for initialization
+	void Start () {
+		if (!monobitView.isMine) {
+			return;
+		}
+		Target = GameObject.Find (TargetName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!monobitView.isMine) {
+			return;
+		}
+		transform.position = Target.transform.position;
+		transform.rotation = Target.transform.rotation;
+	}
+}
